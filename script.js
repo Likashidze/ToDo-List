da6c1d5f3245728508038a353712df415aa41465
lkshkv <103245457+Likashidze@users.noreply.github.com>
2022-05-25 14:32:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+const dom = {
+    new:document.getElementById('new'),
+    add:document.getElementById('add'),
+    tasks: document.getElementById('tasks'),
+    count: document.getElementById('count')
+}
+const tasks = [];
+
+//Отслеживаем клики по кнопке Добавить задачу
+dom.add.onclick = () => {
+    const newTaskText = dom.new.value
+    if(newTaskText && isNotHaveTask(newTaskText, tasks)) { 
+        addTask(newTaskText, tasks)
+        dom.new.value = ''
+        tasksRender(tasks)
+    }
+}
+
+//Функция добавления задачи
+function addTask(text, list) {
+    const timestamp = Date.now()
+    const task = {
+        id: timestamp,
+        text,
+        isComplete: false
+    }
+    list.push(task)
+}
+
+//Проверка существования задачи в массиве задач
+function isNotHaveTask(text, list) {
+    let isNotHave = true
+
+    list.forEach((task) => {
+        if(task.text == text) {
+        alert('Задача уже существует!')
+        isNotHave = false
+        }
+    })
+
+    return isNotHave
+    }
+
+//Функция вывода списка задач
+function tasksRender(list) {
+    let htmlList = ''
+
+    list.forEach((task) => {
+        const cls = task.isComplete 
+        ? 'todo__task todo__task_complete'
+        : 'todo__task'
+            
+        const checked = task.isComplete ? 'checked' : ''
+            
+        const taskHtml = `
+        <div id="${task.id}" class="${cls}">
+            <label class="todo__checkbox">
+                <input type="checkbox" ${checked}>
+                <div class ="todo__checkbox-div"></div>
+            </label>
+            <div class="todo__task-text">${task.text}
+            </div>
+            <div class="todo__task-del">-</div>
+        </div>
+        `
+
+        htmlList = htmlList + taskHtml
+    })
+
+    dom.tasks.innerHTML = htmlList
+
+    renderTasksCount(list)
+}
+
+
+//Отслеживаем клик по чекбоксу задачи
+dom.tasks.onclick = (event) => {
+    const target = event.target
+    const isCheckboxEl = target.classList.contains('todo__checkbox-div')
+    const isDeleteEl = target.classList.contains('todo__task-del')
+
+    if (isCheckboxEl) {
+        const task = target.parentElement.parentElement
+        const taskId = task.getAttribute('id')
+        changeTasksStatus(taskId, tasks)
+        tasksRender(tasks)
+    }
+    if (isDeleteEl) {
+        const task = target.parentElement
+        const taskId = task.getAttribute('id')
+        deleteTask(taskId, tasks)
+        tasksRender(tasks)
+        console.log(tasks.length)
+    }  
+}
+
+//Функции изменения статуса задачи
+function changeTasksStatus(id, list) {
+    list.forEach((task) => {
+        if(task.id == id) {
+        task.isComplete = !task.isComplete
+        }
+    })
+}
+
+//Функция удаления задачи
+function deleteTask(id, list) {
+    list.forEach((task, idx) => {
+        if (task.id == id) {
+        list.splice(idx, 1)
+        }
+    })
+}
+
+//Вывод количества задач
+function renderTasksCount(list) {
+    dom.count.innerHTML = list.length
+}
+    
